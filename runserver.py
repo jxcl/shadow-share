ae9b5177e4cd08615ba513597bc29ef1bc2d8d7c
Alexander Bliskovsky <alex@bethesdatech.net>
2014-04-05 11:04:06 -0400

    Fixed variable name bug.

--- runserver.py
+++ runserver.py
@@ -1,3 +1,9 @@
 from server import main
+import os
+
+upload_folder = main.app.config['UPLOAD_FOLDER']
+
+if not os.path.exists(upload_folder):
+    os.mkdir(upload_folder)
 
 main.app.run()
--- server/main.py
+++ server/main.py
@@ -4,14 +4,16 @@ import sqlite3
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads'
-
+app.debug = True
 
 @app.route("/<user_name>/store/", methods=["POST"])
 def store(user_name):
     if request.method == 'POST':
+        print(request.files)
         f = request.files['file']
         f.save(path.join(app.config['UPLOAD_FOLDER'],
-                         "{}.gpg".format(username)))
+                         "{}.gpg".format(user_name)))
+        return "SUCCESS"
 
 @app.route("/<user_name>/retrieve/")
 def retrieve(user_name):
