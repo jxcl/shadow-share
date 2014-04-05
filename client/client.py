901708169cd46dca6837948e5834289b66d99ee8
William Weston-Dawkes <willwd@terpmail.umd.edu>
2014-04-05 14:31:25 -0400

    Store and Retrieve working with command line arguments

--- client/client.py
+++ client/client.py
@@ -1,16 +1,52 @@
 import json
 import requests
 import argparse
+import base64
 
 def store(user_name, file_name):
     url = 'http://localhost:5000/{}/store/'.format(user_name)
-    files = {'file': open(file_name,'rb')}
-    r = requests.post(url,files=files)
-    print(r.text)
+    
+    with open(file_name, "rb") as fp:
+        bts = fp.read()
+        b64_bytes = base64.b64encode(bts).decode("utf-8")
+        
+        payload = {
+            "file_name": file_name,
+            "file_data": b64_bytes
+            }
+    
+    print("Flag: " + json.dumps(payload))
+    headers = {'content-type': 'application/json'}
+    r = requests.post(url,data=json.dumps(payload), headers=headers)
 
+def retrieve(user_name):
+    url = 'http://localhost:5000/{}/store/'.format(user_name)
+    r = requests.get(url)
+    req_obj = r.json()
+    if req_obj['message'] == 'SUCCESS':
+        print('SUCCESSFULLY Retrieved')
+        file_data = base64.b64decode(req_obj["file_data"])
+        file_name = req_obj["file_name"]
+        
+        with open(file_name,"wb") as fp:
+            fp.write(file_data)
+    elif:
+        print('Failed')
 
-parse = argparse.ArgumentParser()
-parse.add_argument("user_name")
-parse.add_argument("file_name")
-args = parse.parse_args()
-store(args.user_name, args.file_name)
+parser = argparse.ArgumentParser()
+subparsers = parser.add_subparsers(dest='command')
+
+parser_store = subparsers.add_parser('store')
+parser_store.add_argument('user_name')
+parser_store.add_argument('file_name')
+
+parser_retrieve = subparsers.add_parser('retrieve')
+parser_retrieve.add_argument('user_name')
+
+args = parser.parse_args()
+#print(args_p)
+if args.command == "store":
+    store(args.user_name, args.file_name)
+elif args.command == "recieve":
+    recieve(args.user_name)
+
