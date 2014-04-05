d3bba467038d280be1a1038b8f330d694baf0d72
Alexander Bliskovsky <alex@bethesdatech.net>
2014-04-05 10:56:42 -0400

    Fixed url in client.

--- client/client.py
+++ client/client.py
@@ -2,8 +2,9 @@ import json
 import requests
 
 def store():
-    url = 'http://localhost:5000/will/store'
+    url = 'http://localhost:5000/will/store/'
     files = {'file': ('report.csv', 'some,data,to,send\nanother,row,to,send\n')}
     r = requests.post(url,files=files)
-    
+    print(r.text)
+
 store()
