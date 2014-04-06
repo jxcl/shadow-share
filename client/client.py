1076c2c6a4f755aa931c5e61a3cf5fb7b02177b0
Alexander Bliskovsky <alex@bethesdatech.net>
2014-04-05 22:48:44 -0400

    Removed enigshare.db

--- client/client.py
+++ client/client.py
@@ -18,12 +18,12 @@ def store(user_name, file_name):
             if s == user_name:
                 b64_bytes = base64.b64encode(bts).decode("utf-8")
                 # encrypt with gpg
-                
+
                 payload = {
                     "file_name": file_name,
                     "file_data": b64_bytes
                     }
-        
+
     headers = {'content-type': 'application/json'}
     r = requests.post(url,data=json.dumps(payload), headers=headers)
 
@@ -35,7 +35,7 @@ def retrieve(user_name):
         print('SUCCESSFULLY Retrieved')
         file_data = base64.b64decode(req_obj["file_data"])
         file_name = req_obj["file_name"]
-        
+
         with open(file_name,"wb") as fp:
             fp.write(file_data)
     else:
@@ -43,21 +43,21 @@ def retrieve(user_name):
 
 def register(user_name):
     url = 'http://localhost:5000/{}/register/'.format(user_name)
-    g = gnupg.GPG(gnupghome='gnupg') 
+    g = gnupg.GPG(gnupghome='gnupg')
     input_data = g.gen_key_input(
-        key_type="RSA", 
-        key_length = 512, 
+        key_type="RSA",
+        key_length = 512,
         name_real = user_name
         )
 
-    armoured_pub_key =g.export_keys('8FD14534C087AA66') 
+    armoured_pub_key =g.export_keys('B0402C72CCDDF8A2')
     payload = {
         "user_name" : user_name,
         "public_key" : armoured_pub_key
         }
     headers = {'content-type': 'application/json'}
     r = requests.post(url,data=json.dumps(payload), headers=headers)
-    print(r.text)
+    print(r.json())
 
 def get_key(user_name):
     url = 'http://localhost:5000/{}/register/'.format(user_name)
@@ -65,7 +65,7 @@ def get_key(user_name):
     req_obj = r.json()
     if req_obj['status'] == 'SUCCESS':
         print('Get_Key Succeeded')
-        g = gnupg.GPG(gnupghome='gnupg') 
+        g = gnupg.GPG(gnupghome='gnupg')
         g.import_keys(req_obj['public_key'])
     else:
         print('Failed: {}'.format(req_obj['error_message']))
