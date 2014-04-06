7f4a0570f2a9674c0837e144205d98bc65dc8ad7
Alexander Bliskovsky <alex@bethesdatech.net>
2014-04-06 03:03:19 -0400

    Added list to key utility

--- keyutil.py
+++ keyutil.py
@@ -10,9 +10,14 @@ def clean_keys(gpg):
         if key['uids'][0] != secret_key_uid:
             gpg.delete_keys(key['fingerprint'])
 
+def list_keys(gpg):
+    print gpg.list_keys()
+
 if __name__ == "__main__":
     gpg = gnupg.GPG(gnupghome="gnupg")
 
     if len(sys.argv) > 1:
         if sys.argv[1] == "clean":
             clean_keys(gpg)
+        elif sys.argv[1] == "list":
+            list_keys(gpg)
